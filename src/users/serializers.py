ff9230e25dfb7d960f7dcc644f266dd37dba56c4
Konlawat Hutsaithong <konlawatit@gmail.com>
2022-04-17 16:43:53 +0700

    implement password change function

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -20,6 +20,20 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+    
+    def update(self, instance, validated_data):
+        try:
+            user = instance
+            password = validated_data.pop("password")
+            old_password = validated_data.pop("old_password")
+            if user.check_password(old_password):
+                user.se_password(password)
+            else:
+                raise Exception("Old password is incorrect.")
+            user.save()
+        except Exception as err:
+            raise serializers.ValidationError({"info": err})
+        return super(UserSerializer, self).update(instance, validated_data)
 
     class Meta:
         model = User
