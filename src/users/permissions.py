ec6c755d6211ee4da79a6842e1ed73c9f199d800
Konlawat Hutsaithong <konlawatit@gmail.com>
2022-04-17 17:55:47 +0700

    custom profile viewset function

--- src/users/permissions.py
+++ src/users/permissions.py
@@ -14,3 +14,18 @@ class IsUserOwnerOrGetAndPostOnly(permissions.BasePermission):
         if not request.user.is_anonymous:
             return request.user == obj
         return False
+
+class IsProfileOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission for ProfileViewSet to only allow user to edit their own profile. Otherwise, Get and Post only.
+    """
+
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if not request.user.is_anonymous:
+            return request.user.profile == obj
+        return False
--- src/users/viewsets.py
+++ src/users/viewsets.py
@@ -1,16 +1,23 @@
 from django.contrib.auth.models import User
 
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from .serializers import UserSerializer, ProfileSerializer
 
+from .permissions import IsProfileOwnerOrReadOnly, IsUserOwnerOrGetAndPostOnly
+
 from .models import Profile
-from users import serializers
+from users import permissions, serializers
+
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_class = [IsUserOwnerOrGetAndPostOnly,]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class ProfileViewSet(viewsets.ModelViewSet):
+class ProfileViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+    #mixins.ListModelMixin, mixins.CreatetModelMixin, mixins.DestroyModelMixin,
+):
+    permission_class = [IsProfileOwnerOrReadOnly,]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
